78bbe46d633d32350e5debe8db47ec702d6de61a
PeterWibroeLeth <pelet24@student.sdu.dk>
2024-10-22 11:47:33 +0200

    Added core files

---
+++ World Of Zuul Core files/CommandExit.cs
@@ -0,0 +1,9 @@
+/* Command for exiting program
+ */
+
+class CommandExit : BaseCommand, ICommand {
+  public void Execute (Context context, string command, string[] parameters) {
+    context.MakeDone();
+  }
+}
+ 
